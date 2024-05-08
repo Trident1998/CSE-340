488715e2bdcad6c86b74083f2ccc66b958737136
Dmytro <dima4994@gmail.com>
2024-05-08 18:19:48 +0300

    deleted file

--- render.com db.session.sql
+++
@@ -1,4 +0,0 @@
-UPDATE inventory
-SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
-    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/')
-WHERE inv_image LIKE '/images/%' AND inv_thumbnail LIKE '/images/%';
